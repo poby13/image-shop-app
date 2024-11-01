53281ae015045e0815f51bf10ad797a2329d543a
shinis(home) <actionshin@gmail.com>
2024-11-01 21:09:19 +0900

    프로젝트 구성

---
+++ src/main/java/kr/co/cofile/sbimgshop/SbImgShopApplication.java
@@ -0,0 +1,13 @@
+package kr.co.cofile.sbimgshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbImgShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbImgShopApplication.class, args);
+    }
+
+}
