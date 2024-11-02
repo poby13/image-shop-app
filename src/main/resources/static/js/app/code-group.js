c745be07749253a2c2bba24fe3e6e2b42cb7c0c0
shinis(home) <actionshin@gmail.com>
2024-11-03 03:07:04 +0900

    그룹코드 등록시 유효성 오류를 각 필드에 표시

--- src/main/resources/static/js/app/code-group.js
+++ src/main/resources/static/js/app/code-group.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $("#codeGroupListId").click(function () {
         console.log("codeGroupList");
     })
@@ -8,6 +8,9 @@ $(document).ready(function() {
     })
 
     $("#codeGroupRegisterId").click(function () {
+        // 이전 에러 표시 초기화
+        clearErrors();
+
         const codeGroupObj = {
             groupCode: $("#groupCodeId").val(),
             groupName: $("#groupNameId").val(),
@@ -22,9 +25,24 @@ $(document).ready(function() {
             },
             body: JSON.stringify(codeGroupObj)
         })
-            .then(response => response.json())
+            .then(response => {
+                if (!response.ok) {
+                    return response.json().then(errorData => {
+                        throw errorData;
+                    })
+                }
+                response.json()
+            })
             .then(data => console.log(data))
-            .catch(error => console.error('Error:', error));
+            .catch(error => {
+                console.error('Error:', error);
+                if (error.errors) {
+                    showErrors(error.errors);
+                } else {
+                    // 일반적인 에러 처리
+                    alert(error.message || '처리 중 오류가 발생했습니다.');
+                }
+            });
     })
 
     $("#codeGroupDeleteId").click(function () {
