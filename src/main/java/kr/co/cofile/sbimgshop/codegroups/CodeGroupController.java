e64e3e713a28f47b31b8b02ed8cfc86fab3ca2a1
shinis(home) <actionshin@gmail.com>
2024-11-03 02:17:29 +0900

    요청객체 유효성 검사

--- src/main/java/kr/co/cofile/sbimgshop/codegroups/CodeGroupController.java
+++ src/main/java/kr/co/cofile/sbimgshop/codegroups/CodeGroupController.java
@@ -1,8 +1,13 @@
 package kr.co.cofile.sbimgshop.codegroups;
 
+import jakarta.validation.Valid;
+import kr.co.cofile.sbimgshop.common.exception.BusinessException;
+import kr.co.cofile.sbimgshop.common.exception.CustomValidationException;
+import kr.co.cofile.sbimgshop.common.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +22,13 @@ public class CodeGroupController {
     private final CodeGroupService codeGroupService;
 
     @PostMapping
-    public ResponseEntity<CodeGroupDTO> register(@RequestBody CodeGroupDTO codeGroupDTO) {
+    public ResponseEntity<CodeGroupDTO> register(@Valid @RequestBody CodeGroupDTO codeGroupDTO,
+                                                 BindingResult bindingResult) {
         log.info("register: {}", codeGroupDTO);
+
+        if (bindingResult.hasErrors()) {
+            throw new CustomValidationException(ErrorCode.VALIDATION_ERROR, bindingResult);
+        }
 
         codeGroupService.register(codeGroupDTO);
 
--- src/main/java/kr/co/cofile/sbimgshop/codegroups/CodeGroupDTO.java
+++ src/main/java/kr/co/cofile/sbimgshop/codegroups/CodeGroupDTO.java
@@ -1,6 +1,8 @@
 package kr.co.cofile.sbimgshop.codegroups;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -11,9 +13,15 @@ import java.time.LocalDateTime;
 @Setter
 @ToString
 public class CodeGroupDTO {
+    @NotBlank
+    @Size(min = 3, max = 3)
     private String groupCode;
+
+    @NotBlank
+    @Size(min = 3, max = 3)
     private String groupName;
-    private String useYn;
+
+    private String useYn = "Y";
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime createdAt;
