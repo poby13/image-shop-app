3e890c341c72230a0eebf077ee275162488a2682
shinis(home) <actionshin@gmail.com>
2024-11-03 02:10:55 +0900

    예외처리-유효성검사

--- src/main/java/kr/co/cofile/sbimgshop/common/exception/GlobalExceptionHandler.java
+++ src/main/java/kr/co/cofile/sbimgshop/common/exception/GlobalExceptionHandler.java
@@ -3,10 +3,12 @@ package kr.co.cofile.sbimgshop.common.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 @Slf4j
 @ControllerAdvice
@@ -18,6 +20,19 @@ public class GlobalExceptionHandler {
         log.error("Business Exception", e);
 
         ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+
+    @ExceptionHandler(CustomValidationException.class)
+    protected ResponseEntity<ErrorResponse> handleValidationException(CustomValidationException e) {
+        log.error("Validation Exception", e);
+
+        BindingResult bindingResult = e.getBindingResult();
+        Map<String, String> errors = ErrorResponse.bindingResultToMap(bindingResult);
+
+        ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode(), errors);
 
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
