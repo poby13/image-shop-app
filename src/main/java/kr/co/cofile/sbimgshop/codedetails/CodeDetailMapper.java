b13d33b186be29b4c185c3f1dfe3091d997a5e64
shinis(ys) <actionshin@gmail.com>
2024-11-27 11:39:03 +0900

    민감정보 관리방식 변경

---
+++ src/main/java/kr/co/cofile/sbimgshop/codedetails/CodeDetailMapper.java
@@ -0,0 +1,25 @@
+package kr.co.cofile.sbimgshop.codedetails;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public class CodeDetailMapper {
+
+    // insert
+
+    // selectAll
+
+    // selectByCondition
+
+    // selectByGroupCodeAndCodeValue
+
+    // exists
+
+    // delete
+
+    // countTotal
+
+    // updateCodeDetail
+
+    // partialUpdateCodeDetail
+}
